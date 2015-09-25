345aa6b7e849c8ba91382c1e2284e051b1d21d77
TesX <tesfabpel@gmail.com>
2015-09-25 10:43:30 +0200

    Now the socket gets open and closed for each command

--- srcon.rb
+++ srcon.rb
@@ -12,115 +12,191 @@ module Rcon
 		SERVERDATA_RESPONSE_VALUE = 0
 	end
 
+	def self.get_sock
+		host = $config[:host]
+		port = $config[:port]
+
+		TCPSocket.new host, port
+	end
+
 	def self.send_to(packet, sock)
 		szb = [packet[:size]].pack 'l<'
 		idb = [packet[:id]].pack 'l<'
-		typeb = [packet[:type]].pack 'l<'
-		bodyb = [packet[:body]].pack 'Z*'
-		msg = szb+idb+typeb+bodyb+"\0"
+		type_b = [packet[:type]].pack 'l<'
+		body_b = [packet[:body]].pack 'Z*'
+		msg = szb + idb + type_b + body_b + "\0"
 
 		sock.sendmsg(msg)
 	end
 
 	def self.recv_from(sock)
-		msgary = sock.recvmsg()
-		msg = msgary[0]
+		msg_ary = sock.recvmsg
+		msg = msg_ary[0]
 		ary = msg.unpack('l<l<l<Z*')
 
 		RconPacket.new(ary[0],ary[1],ary[2],ary[3])
 	end
 
+	def self.send_recv(sock, msg, type = Rcon::PacketType::SERVERDATA_EXECCOMMAND)
+		p = Rcon::RconPacket.new(10+msg.bytesize, 0, type, msg)
+		Rcon::send_to(p, sock)
+		pr = Rcon::recv_from(sock)
+		pr
+	end
+
+	def self.parse_options
+		config = {
+			password_tmp: nil,
+			password_verified: false,
+		}
+
+		op = OptionParser.new do |opts|
+			opts.banner = "Usage: ruby srcon.rb host port [-p -|password] [-- command]"
+
+			opts.separator ''
+
+			opts.on('-p PASSWORD', 'The password') do |pass|
+				if pass.nil?
+					STDERR.puts 'ERROR: You need to specify a password if -p is present.'
+					opts.terminate
+					exit
+				end
+
+				pass = :stdin if pass == '-'
+
+				config[:password] = pass
+			end
+		end
+
+		if ARGV.length < 2
+			STDERR.puts "ERROR! Missing required arguments."
+			STDERR.puts op.help
+			exit false
+		end
+
+		config[:host] = ARGV.shift
+		config[:port] = ARGV.shift.to_i
+
+		op.parse ARGV
+
+		# Parse the -- argument
+		cmdi = ARGV.index '--'
+		unless cmdi.nil?
+			cmd_ary = ARGV[cmdi+1 .. -1]
+
+			if cmd_ary.nil?
+				STDERR.puts 'ERROR: Command is empty.'
+				exit
+			end
+			cmd = cmd_ary.join(' ')
+			config[:command] = cmd
+		end
+
+		config
+	end
+
+	def self.ask_pass
+
+		return if $config[:password_verified]
+
+		pass = $config[:password]
+
+		if pass.nil?
+			# Ask using readline
+			pass = Readline.readline('Password> ')
+
+			if pass.nil? #EOF
+				puts
+				return false
+			end
+		elsif pass == :stdin
+			# Read directly from stdin
+			pass = gets.chomp
+		end
+
+		$config[:password_tmp] = pass
+		$config[:password_verified] ||= false
+	end
+
 	def self.auth(sock)
-		pass = Readline.readline('Password> ')
-		if pass.nil? #EOF
-			puts
+		pass = $config[:password_tmp] || $config[:password]
+
+		pr = Rcon::send_recv(sock, pass, Rcon::PacketType::SERVERDATA_AUTH)
+		if pr[:id] == -1
+			$config[:password_verified] = false
 			return false
 		end
 
-		p = Rcon::RconPacket.new(10+pass.bytesize, 0, Rcon::PacketType::SERVERDATA_AUTH, pass)
-		Rcon::send_to(p, sock)
-		pr = Rcon.recv_from(sock)
-		if pr[:id] == -1
-			return false
-		end
+		$config[:password] = pass
+		$config[:password_tmp] = nil
+		$config[:password_verified] = true
 
 		true
 	end
 
-	def self.looper(sock)
-		loop do
+	def self.single_command
+
+		begin
+			Rcon::ask_pass
+
 			line = Readline.readline('rcon> ', true)
 			if line.nil? #EOF
 				puts
-				break
+				raise new EOFError
 			end
 
-			p = Rcon::RconPacket.new(10+line.bytesize, 0, Rcon::PacketType::SERVERDATA_EXECCOMMAND, line)
-			Rcon::send_to(p, sock)
-			pr = Rcon.recv_from(sock)
-			puts pr[:body]
+			sock = Rcon::get_sock()
+
+			begin
+				#puts "OPEN"
+
+				unless Rcon::auth(sock)
+					raise "Authentication Error"
+				end
+
+				pr = Rcon::send_recv(sock, line)
+				puts pr[:body]
+			rescue
+				p $!
+			end
+
+			#puts "CLOSE"
+			sock.close
+
+		rescue EOFError => e
+			raise e
+
+		end
+
+	end
+
+	def self.looper
+		loop do
+			Rcon::single_command
 		end
 	end
 end
 
 def main
-	config = {}
-	op = OptionParser.new do |opts|
-		opts.banner = "Usage: ruby srcon.rb host port [-p -|password] [-- command]"
+	$config = Rcon::parse_options
 
-		opts.separator ''
+	begin
+		cmd = $config[:command]
 
-		opts.on('-p [password]', 'The password') do |pass|
-			if pass.nil?
-				STDERR.puts 'ERROR: You need to specify a password if -p is present.'
-				opts.terminate
-				exit
-			end
-
-			pass = nil if pass == '-'
-
-			config[:password] = pass
+		# Do we already have a command?
+		if cmd.nil?
+			Rcon::looper
+		else
+			Rcon::single_command
 		end
-	end
-
-	if ARGV.length < 2
-		STDERR.puts "ERROR! Missing required arguments."
-		STDERR.puts op.help
+	rescue EOFError
+		# ignored
+	rescue
+		p $!
 		exit false
 	end
 
-	host = ARGV.shift
-	port = ARGV.shift.to_i
-
-	op.parse
-
-	# Parse the -- argument
-	cmdi = ARGV.index '--'
-	unless cmdi.nil?
-		cmd_ary = ARGV[cmdi+1 .. -1]
-
-		if cmd_ary.nil?
-			STDERR.puts 'ERROR: Command is empty.'
-			exit
-		end
-		cmd = cmd_ary.join(' ')
-		config[:command] = cmd
-	end
-
-	$sock = TCPSocket.new host, port
-
-	begin
-		unless Rcon.auth($sock)
-			STDERR.puts "AUTH ERROR"
-			exit false
-		end
-
-		Rcon.looper($sock)
-	rescue
-	end
-
-	$sock.close
-
+	exit true
 end
 
 if __FILE__ == $0
